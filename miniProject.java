1eb972c13e78e1da85537fb536e6ad5d69886993
MarkusJoha <112433408+MarkusJoha@users.noreply.github.com>
2022-10-09 12:50:50 +0200

    Add files via upload

---
+++ miniProject.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class miniProject {
+
+    public static void main(String[] args) {
+
+        Random randNum = new Random();
+        int answer = randNum.nextInt(100);
+        System.out.println("Welcome to the Guessing game!");
+        System.out.println("The goal of this game is to guess on an number of 0-100.");
+        System.out.println("If you get it right, the program will congratulate you and write out how many tries it took you.");
+        System.out.println("With that being said, what's your first guess?");
+        int guess = 0;
+        checkAnswer(guess, answer);
+    }
+
+    static void checkAnswer(int guess, int answer) {
+
+        while (guess != answer) {
+
+            Scanner scanner = new Scanner(System.in);
+            guess = scanner.nextInt();
+            if (guess > answer) {
+                System.out.println("Your guess is too high!");
+            } else if (guess < answer) {
+                System.out.println("Your guess is too low!");
+            } else {
+                System.out.println("Congratulations, you've won the game, the answer is: " + answer);
+            }
+
+        }
+    }
+}
